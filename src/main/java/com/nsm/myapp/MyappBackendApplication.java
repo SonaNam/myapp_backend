8f2f972cce6c6f113e0d832221e1076336f09578
Sona Nam <kihiall@gmail.com>
2023-08-11 12:44:53 +0900

    first commit

---
+++ src/main/java/com/nsm/myapp/MyappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nsm.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappBackendApplication.class, args);
+	}
+
+}
